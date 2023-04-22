66bd1c25920fc4042930524b4d333546ddbe0135
Pare Miquel Rosselló Espases <118070663+prosselloe@users.noreply.github.com>
2023-04-23 00:13:20 +0200

    Add files via upload

--- penjat.sql
+++ penjat.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS TblParaules;
+ï»¿DROP TABLE IF EXISTS TblParaules;
 CREATE TABLE TblParaules (Paraula text, IdIdioma text, IdPista integer,
   PRIMARY KEY (Paraula),
   FOREIGN KEY (IdIdioma) REFERENCES TblIdiomes (IdIdioma),
@@ -31,10 +31,10 @@ DROP TABLE IF EXISTS TblIdiomes;
 CREATE TABLE TblIdiomes (IdIdioma text, Idioma text,
   PRIMARY KEY (IdIdioma));
 
-  INSERT INTO TblIdiomes VALUES ("an","aragonés");
-  INSERT INTO TblIdiomes VALUES ("ca","catalán");
-  INSERT INTO TblIdiomes VALUES ("en","inglés");
-  INSERT INTO TblIdiomes VALUES ("es","español (o castellano)");
+  INSERT INTO TblIdiomes VALUES ("an","aragonÃ©s");
+  INSERT INTO TblIdiomes VALUES ("ca","catalÃ¡n");
+  INSERT INTO TblIdiomes VALUES ("en","inglÃ©s");
+  INSERT INTO TblIdiomes VALUES ("es","espaÃ±ol (o castellano)");
   INSERT INTO TblIdiomes VALUES ("eu","euskera");
   INSERT INTO TblIdiomes VALUES ("gl","gallego");
 
@@ -43,42 +43,42 @@ SELECT TblPistes.Pista, TblParaules.Paraula, TblIdiomes.Idioma
   INNER JOIN TblPistes ON (TblPistes.IdPista = TblParaules.IdPista) AND (TblIdiomes.IdIdioma = TblPistes.IdIdioma)
 ORDER BY TblPistes.Pista, TblParaules.Paraula, TblIdiomes.Idioma;
 
-DROP TABLE IF EXISTS TblPaïsos;
-CREATE TABLE TblPaïsos (IdPais2T text, NomOficial text, IdPais3T text, IdPais3N integer, Observacions text,
+DROP TABLE IF EXISTS TblPaisos;
+CREATE TABLE TblPaisos (IdPais2T text, NomOficial text, IdPais3T text, IdPais3N integer, Observacions text,
   PRIMARY KEY (IdPais2T));
 
-  INSERT INTO TblPaïsos VALUES ("AD","Andorra", "AND", 20, "");
-  INSERT INTO TblPaïsos VALUES ("AU","Australia", "AUS", 36, "Incluye las Islas Ashmore y Cartier y las Islas del Mar del Coral.");
-  INSERT INTO TblPaïsos VALUES ("CA","Canadá", "CAN", 124, "");
-  INSERT INTO TblPaïsos VALUES ("ES","España", "ESP", 724, "Códigos obtenidos del idioma nativo (español): España");
-  INSERT INTO TblPaïsos VALUES ("GB","Reino Unido de Gran Bretaña e Irlanda", "GBR", 826, "Debido a que para obtener los códigos ISO no se utilizan las palabras comunes de Reino y Unido, los códigos se han obtenido a partir del resto del nombre oficial.");
-  INSERT INTO TblPaïsos VALUES ("IE","Irlanda", "IRL", 372, "");
-  INSERT INTO TblPaïsos VALUES ("IN","India", "IND", 356, "");
-  INSERT INTO TblPaïsos VALUES ("US","Estados Unidos de América (los)", "USA", 840, "");
+  INSERT INTO TblPaisos VALUES ("AD","Andorra", "AND", 20, "");
+  INSERT INTO TblPaisos VALUES ("AU","Australia", "AUS", 36, "Incluye las Islas Ashmore y Cartier y las Islas del Mar del Coral.");
+  INSERT INTO TblPaisos VALUES ("CA","CanadÃ¡", "CAN", 124, "");
+  INSERT INTO TblPaisos VALUES ("ES","EspaÃ±a", "ESP", 724, "CÃ³digos obtenidos del idioma nativo (espaÃ±ol): EspaÃ±a");
+  INSERT INTO TblPaisos VALUES ("GB","Reino Unido de Gran BretaÃ±a e Irlanda", "GBR", 826, "Debido a que para obtener los cÃ³digos ISO no se utilizan las palabras comunes de Reino y Unido, los cÃ³digos se han obtenido a partir del resto del nombre oficial.");
+  INSERT INTO TblPaisos VALUES ("IE","Irlanda", "IRL", 372, "");
+  INSERT INTO TblPaisos VALUES ("IN","India", "IND", 356, "");
+  INSERT INTO TblPaisos VALUES ("US","Estados Unidos de AmÃ©rica (los)", "USA", 840, "");
 
-DROP TABLE IF EXISTS TblIdiomesPaïsos;
-CREATE TABLE TblIdiomesPaïsos (IdIdioma text, IdPais2T text,
+DROP TABLE IF EXISTS TblIdiomesPaisos;
+CREATE TABLE TblIdiomesPaisos (IdIdioma text, IdPais2T text,
   PRIMARY KEY (IdIdioma, IdPais2T),
   FOREIGN KEY (IdIdioma) REFERENCES TblIdiomes (IdIdioma)
-  FOREIGN KEY (IdPais2T) REFERENCES TblPaïsos (IdPais2T));
+  FOREIGN KEY (IdPais2T) REFERENCES TblPaisos (IdPais2T));
 
-  INSERT INTO TblIdiomesPaïsos VALUES ("an", "ES");
-  INSERT INTO TblIdiomesPaïsos VALUES ("ca", "AD");
-  INSERT INTO TblIdiomesPaïsos VALUES ("ca", "ES");
-  INSERT INTO TblIdiomesPaïsos VALUES ("en", "AU");
-  INSERT INTO TblIdiomesPaïsos VALUES ("en", "CA");
-  INSERT INTO TblIdiomesPaïsos VALUES ("en", "GB");
-  INSERT INTO TblIdiomesPaïsos VALUES ("en", "IE");
-  INSERT INTO TblIdiomesPaïsos VALUES ("en", "IN");
-  INSERT INTO TblIdiomesPaïsos VALUES ("en", "US");
-  INSERT INTO TblIdiomesPaïsos VALUES ("es", "ES");
-  INSERT INTO TblIdiomesPaïsos VALUES ("eu", "ES");
-  INSERT INTO TblIdiomesPaïsos VALUES ("gl", "ES");
+  INSERT INTO TblIdiomesPaisos VALUES ("an", "ES");
+  INSERT INTO TblIdiomesPaisos VALUES ("ca", "AD");
+  INSERT INTO TblIdiomesPaisos VALUES ("ca", "ES");
+  INSERT INTO TblIdiomesPaisos VALUES ("en", "AU");
+  INSERT INTO TblIdiomesPaisos VALUES ("en", "CA");
+  INSERT INTO TblIdiomesPaisos VALUES ("en", "GB");
+  INSERT INTO TblIdiomesPaisos VALUES ("en", "IE");
+  INSERT INTO TblIdiomesPaisos VALUES ("en", "IN");
+  INSERT INTO TblIdiomesPaisos VALUES ("en", "US");
+  INSERT INTO TblIdiomesPaisos VALUES ("es", "ES");
+  INSERT INTO TblIdiomesPaisos VALUES ("eu", "ES");
+  INSERT INTO TblIdiomesPaisos VALUES ("gl", "ES");
 
-SELECT TblIdiomes.IdIdioma, TblIdiomes.Idioma, TblPaïsos.IdPais2T, TblPaïsos.NomOficial
-  FROM TblPaïsos 
-  INNER JOIN (TblIdiomes INNER JOIN TblIdiomesPaïsos ON TblIdiomes.IdIdioma = TblIdiomesPaïsos.IdIdioma) 
-    ON TblPaïsos.IdPais2T = TblIdiomesPaïsos.IdPais2T;
+SELECT TblIdiomes.IdIdioma, TblIdiomes.Idioma, TblPaisos.IdPais2T, TblPaisos.NomOficial
+  FROM TblPaisos 
+  INNER JOIN (TblIdiomes INNER JOIN TblIdiomesPaisos ON TblIdiomes.IdIdioma = TblIdiomesPaisos.IdIdioma) 
+    ON TblPaisos.IdPais2T = TblIdiomesPaisos.IdPais2T;
 
 DROP TABLE IF EXISTS TblTextosGUI;
 CREATE TABLE TblTextosGUI (IdIdioma text, Titol text, Versio text, Input text, Pregunta text, 
@@ -89,24 +89,24 @@ CREATE TABLE TblTextosGUI (IdIdioma text, Titol text, Versio text, Input text, P
   PRIMARY KEY (IdIdioma),
   FOREIGN KEY (IdIdioma) REFERENCES TblIdiomes (IdIdioma));
 
-  INSERT INTO TblTextosGUI VALUES ("ca", "Versió amb Estructures de Dades Joc del Penjat", "Versió ß Joc del Penjat", 
-    "Escriu una lletra minúscula", "Anam a la quinta forca?", "Comprovar", "Paraula:", "Demanes sopes?", "Pista", 
-    "Vides:", "Un moix en té set?", "Lletres:", "Ets de lletres?", "URLpistes:", "Dita", "A la quinta forca,", 
-    "A ca un penjat, no hi anomenis cordes,", "Setze jutges d'un jutjat mengen fetge d'un penjat, …", "Crèdits:", 
-    "Joc Penjat on Scratch", "Penjat", "en Català", "Diccionari", "Mostra o Amaga", "Caràcter incorrecte!", 
-    "Lletra repetida!", "Has encertat!", "i has guanyat!", "Has fallat!", "i has perdut!", "En pau descansi – RIP!", 
-    "Puntuació:");
-  INSERT INTO TblTextosGUI VALUES ("es", "Versión con Estructuras de Datos Juego del Ahorcado", "Versión ß Juego del Ahorcado", 
-    "Escribe una letra minúscula", "Vamos al quinto pino?", "Comprobar", "Palabra:", "Te rindes?", "Pista", 
+  INSERT INTO TblTextosGUI VALUES ("ca", "VersiÃ³ amb Base de Dades Joc del Penjat", "VersiÃ³ Î³ Joc del Penjat", 
+    "Escriu una lletra minÃºscula", "Anam a la quinta forca?", "Comprovar", "Paraula:", "Demanes sopes?", "Pista", 
+    "Vides:", "Un moix en tÃ© set?", "Lletres:", "Ets de lletres?", "URLpistes:", "Dita", "A la quinta forca,", 
+    "A ca un penjat, no hi anomenis cordes,", "Setze jutges d'un jutjat mengen fetge d'un penjat, â€¦", "CrÃ¨dits:", 
+    "Joc Penjat on Scratch", "Penjat", "en CatalÃ ", "Diccionari", "Mostra o Amaga", "CarÃ cter incorrecte!", 
+    "Lletra repetida!", "Has encertat!", "i has guanyat!", "Has fallat!", "i has perdut!", "En pau descansi â€“ RIP!", 
+    "PuntuaciÃ³:");
+  INSERT INTO TblTextosGUI VALUES ("es", "VersiÃ³n con Base de Datos Juego del Ahorcado", "VersiÃ³n Î³ Juego del Ahorcado", 
+    "Escribe una letra minÃºscula", "Vamos al quinto pino?", "Comprobar", "Palabra:", "Te rindes?", "Pista", 
     "Vidas:", "Un gato tiene siete?", "Letras:", "Eres de letras?", "URLpistas:", "Dicho", "Al quinto pino, ", 
-    "En casa de un ahorcado, no hables de cuerdas,", "Dieciséis jueces de un juzgado comen hígado de un ahorcado, …", "Crèditos:", 
-    "Juego Ahorcado on Scratch", "Ahorcado", "en Español", "Diccionario", "Muestra o Esconde", "Carácter incorrecto!", 
+    "En casa de un ahorcado, no hables de cuerdas,", "DiecisÃ©is jueces de un juzgado comen hÃ­gado de un ahorcado, â€¦", "CrÃ¨ditos:", 
+    "Juego Ahorcado on Scratch", "Ahorcado", "en EspaÃ±ol", "Diccionario", "Muestra o Esconde", "CarÃ¡cter incorrecto!", 
     "Letra repetida!", "Has acertado!", "y has ganado!", "Has fallado!", "y has perdido!", "En paz descanse - RIP!", 
-    "Puntuación:" ); 
-  INSERT INTO TblTextosGUI VALUES ("en", "Hangman Game Images and Sounds Version", "Hangman Game ß Versión", 
+    "PuntuaciÃ³n:" ); 
+  INSERT INTO TblTextosGUI VALUES ("en", "Hangman Game Database Version", "Hangman Game Î³ VersiÃ³n", 
     "Write a lowercase letter", "Are we going to the boondocks?", "Check it", "Word:", "You give up?", "Clue", 
     "Lives:", "A cat has seven?", "Letters:", "Are you in liberal arts?", "URLclues:", "Saying", "To the boondocks,", 
-    "In a hanged man's home, don't talk about ropes,", "Sixteen judges of a court eat the liver of a hangman, …", "Credits:", 
+    "In a hanged man's home, don't talk about ropes,", "Sixteen judges of a court eat the liver of a hangman, â€¦", "Credits:", 
     "Hangman Game on Scratch", "Hangman", "in English", "Dictionary", "Show or Hide", "Wrong character!", "Repeated letter!", 
     "You're right!", "and you have won!", "You have failed!", "and you have lost!", "Rest in peace - RIP!", 
     "Score:" ); 
@@ -114,3 +114,16 @@ CREATE TABLE TblTextosGUI (IdIdioma text, Titol text, Versio text, Input text, P
 SELECT TblTextosGUI.IdIdioma, TblTextosGUI.Input, TblTextosGUI.Pregunta, TblTextosGUI.Comprovar, TblTextosGUI.Paraula, 
     TblTextosGUI.Sopes, TblTextosGUI.Pista, TblTextosGUI.Vides, TblTextosGUI.Moix, TblTextosGUI.Lletres, TblTextosGUI.Ets
   FROM TblTextosGUI;
+
+SELECT TblParaules.IdIdioma, Count(TblParaules.Paraula) AS CuentaDeParaula
+  FROM TblParaules
+GROUP BY TblParaules.IdIdioma;
+
+SELECT Count(TblIdiomesPaisos.IdIdioma) AS CuentaDeIdIdioma, TblIdiomesPaisos.IdPais2T
+  FROM TblIdiomesPaisos
+GROUP BY TblIdiomesPaisos.IdPais2T
+HAVING (((Count(TblIdiomesPaisos.IdIdioma))>1));
+
+SELECT DISTINCT TblPistes.Pista, TblPistes.IdIdioma
+  FROM TblPistes 
+INNER JOIN TblParaules ON TblPistes.IdPista = TblParaules.IdPista;
